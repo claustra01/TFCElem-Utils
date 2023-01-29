42732fe7a7d1156cc6784edfbba77071ab5b0396
sny430 <shinya430.g@gmail.com>
2023-01-29 19:30:49 +0900

    :children_crossing: set lead and platinum color

--- tfce_types.py
+++ tfce_types.py
@@ -36,8 +36,8 @@ grades = [
 
 metals = [
     # name, enableTool, enablePart, enableArmor, enableUtility, [textureType(iron/silver/steel), h, s, v]
-    ["lead", False, True, False, False, ["iron", -148, 18, 0]],
-    ["platinum", False, True, False, False, ["silver", -148, 18, 0]],
+    ["lead", False, True, False, False, ["iron", -123, 25, -28]],
+    ["platinum", False, True, False, False, ["silver", -148, 46, 0]],
     ["electrum", False, True, False, False, ["silver", -148, 18, 0]],
     ["invar", False, True, False, False, ["iron", -148, 18, 0]],
     ["constantan", False, True, False, False, ["iron", -148, 18, 0]],
