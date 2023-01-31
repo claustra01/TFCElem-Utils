a1b968802cef4aef8dd267a1191b0adc6281983c
sny430 <shinya430.g@gmail.com>
2023-02-01 00:12:47 +0900

    :bug: fix item path

--- recipe.py
+++ recipe.py
@@ -10,11 +10,11 @@ def anvil_recipe():
             # sheet
             file_path = "../src/main/resources/data/tfc/recipes/anvil/" + metal[0] + "_sheet.json"
             input = "forge:double_ingots/" + metal[0]
-            output = "forge:sheet/" + metal[0]
+            output = "tfc:metal/sheet/" + metal[0]
             tfce_utils.create_anvil_recipe(file_path, input, output, 1, metal[6][0], ["hit_last", "hit_second_last", "hit_third_last"])            
 
             # rod
             file_path = "../src/main/resources/data/tfc/recipes/anvil/" + metal[0] + "_rod.json"
             input = "forge:ingots/" + metal[0]
-            output = "forge:rod/" + metal[0]
+            output = "tfc:metal/rod/" + metal[0]
             tfce_utils.create_anvil_recipe(file_path, input, output, 2, metal[6][0], ["bend_last", "draw_second_last", "draw_third_last"])
